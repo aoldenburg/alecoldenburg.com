b1267354f3cc2172c4f565ced7ffd0f2fd0bd74f
Alec Oldenburg <alecoldenburg@Alecs-MBP.domain>
2018-09-25 22:01:13 -0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import styles from './App.module.css';
+
+export default function App() {
+  return (
+    <div className={styles.App}>
+      <h1>Welcome to alecoldenburg.com</h1>
+    </div>
+  );
+}
